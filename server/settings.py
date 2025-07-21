9d73e7572f5f6cb3e970e4255ba0073436b693ce
Purnima Annam <purnimaannam20@gmail.com>
2025-07-21 21:40:35 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7sxp&27jh2km@d8$8%=)k)74h=-ezlp229i9mkuwxl%u*o8b)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eye_disease'
+    'eye_disease',
 ]
 
 MIDDLEWARE = [
